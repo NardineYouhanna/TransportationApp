827d3864e8bfa13228050b74a5cb83c9b9d184f7
NardineYouhanna <64501661+NardineYouhanna@users.noreply.github.com>
2021-12-31 21:35:35 +0200

    Update Trip.java

--- Trip.java
+++ Trip.java
@@ -1,4 +1,12 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
+
+
 
 public class Trip {
 
@@ -6,7 +14,15 @@ public class Trip {
 	public String destination;
 	public int tripid=0;
 	public int fees;
-	
+	public String dname;
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
 	public String getSource() {
 		return source;
 	}
@@ -14,7 +30,11 @@ public class Trip {
 	public void setSource(String source) {
 		this.source = source;
 	}
-
+	
+	public void endRide() {
+		System.out.println(" Ride has ended , please pay " + this.getFees());
+	}
+    
 	public String getDestination() {
 		return destination;
 	}
@@ -49,4 +69,16 @@ public class Trip {
 		}
 		return tt;
 	}
+	//9) 10}The system should calculate the distance and estimate time arrival (ETA) with the help of google maps API
+	public String getDistance(String source, String destination) throws IOException, InterruptedException {
+		final String API_KEY="AIzaSyCCWGB4tDZFGtBA7H22XRKWiLe_jo6YQIg";
+		String url="https://maps.googleapis.com/maps/api/distancematrix/json?origins="+source+"&destinations="+destination+"&key="+ API_KEY;
+       
+
+		HttpClient client =  HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+		HttpResponse<String> response =  client.send(request, BodyHandlers.ofString());
+		return response.body().toString();
+	}
+
 }
